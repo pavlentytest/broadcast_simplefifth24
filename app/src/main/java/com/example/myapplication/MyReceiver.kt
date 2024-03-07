0c8c289fb9ee3b51a190ba548d9136602f76e4ff
Pavel Stepanov <impetus@yandex.ru>
2024-03-07 10:05:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MyReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val flag = intent.getBooleanExtra("state",false)
+        if(flag) {
+            Toast.makeText(context,"Airmode is on!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(context, "Airmode is off!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
